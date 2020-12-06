63e376a1906bfa4fef88aef84e98f3214a5ebaa7
NingamerX <70814662+NingamerX@users.noreply.github.com>
2020-12-06 10:55:09 -0600

    Add files via upload

---
+++ Jet-Fighter-master/sketch.js
@@ -0,0 +1,67 @@
+let blackJet;
+let whiteJet;
+var backgroundimg
+
+let blackJetImage;
+let whiteJetImage;
+const ROTATE_AMOUNT = 0.05;
+
+function preload() {
+	blackJetImage = loadImage("black-jet.png");
+  whiteJetImage = loadImage("white-jet.png");
+  backgroundimg = loadImage("background.jpg")
+}
+
+function setup() {
+  createCanvas(400, 400);
+  blackJet = new Jet(blackJetImage, false);
+  whiteJet = new Jet(whiteJetImage, true);
+}
+
+function draw() {
+  background(backgroundimg);
+  
+  // add the enemy bullets into the update function
+  blackJet.update(whiteJet);
+  whiteJet.update(blackJet);
+  
+  blackJet.draw();
+  whiteJet.draw(); 
+  
+  
+  textSize(50);
+  fill(0);
+  text(blackJet.score, 100, 50);
+  
+  fill(255);
+  text(whiteJet.score, 270, 50);
+}
+
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+  	blackJet.rotateAmount = ROTATE_AMOUNT;
+  } else if (keyCode === LEFT_ARROW) {
+  	blackJet.rotateAmount = -ROTATE_AMOUNT;
+  } else if (keyCode === 68) {
+    // d
+		whiteJet.rotateAmount = ROTATE_AMOUNT;
+  } else if (keyCode === 65) { 
+  	// a
+    whiteJet.rotateAmount = -ROTATE_AMOUNT;
+  } else if (keyCode === 17) {
+  	// ctrl
+    blackJet.shoot();
+  } else if (keyCode === 32) {
+  	// spacebar
+    whiteJet.shoot();
+  }
+}
+
+function keyReleased() {
+	if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+  	blackJet.rotateAmount = 0;
+  } else if (keyCode === 65 || keyCode === 68) {
+  	whiteJet.rotateAmount = 0;
+  }
+}
